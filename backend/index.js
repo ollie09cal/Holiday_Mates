24656d584fc1e321fe6c5c2721519bdd8d3dde5b
Oliver Calvey <ollie.calvey@gmail.com>
2022-02-10 10:29:34 +0000

    user schema start and server setup

---
+++ backend/index.js
@@ -0,0 +1,38 @@
+import express from 'express'
+import mongoose from 'mongoose'
+//import router after routes are setup
+import { port, dbURI } from './config/enviroment.js'
+
+const app = express()
+
+//server setup
+const startServer = async () => {
+  try {
+    await mongoose.connect(dbURI)
+    console.log('✅ mongoDB server connected ✅')
+
+    //JSON parser
+    app.use(express.json())
+
+    //Middleware
+    //logger
+    app.use((req, _res, next) => {
+      console.log(`🚩 request has been recieved ${req.method} - ${req.url}`)
+      next()
+    })
+    //routes //add this in once routes are made
+    // app.use(router)
+
+    //CATCH all
+    app.use((_req, res) => {
+      return res.status(404).json({ message: "route not found" })
+    })
+
+    app.listen(port, () => console.log(`server is listening on port ${port}`))
+
+  } catch (err) {
+    console.log(err)
+  }
+  
+}
+startServer()
---
+++ backend/models/user.js
@@ -0,0 +1,54 @@
+import mongoose from 'mongoose'
+import uniqueValidator from 'mongoose-unique-validator'
+import bcrypt from 'bcrypt'
+
+const { Schema } = mongoose
+const userSchema = new Schema({
+  username: { type: String, required: true, unique: true, maxlength: 30 },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  profilePhoto: { type: String },
+  matesToken: [{ type: String }],
+  personalToken: { type: String, required: true, unique: true, minLength: 10, maxlength: 20 }
+})
+
+//fields for tying holiday cards to user 
+
+userSchema.set('toJSON', {
+  virtuals: true,
+  transform(_doc, json){
+    delete json.password
+    return json
+  }
+})
+
+userSchema
+  .virtual('passwordConfirmation')
+  .set(function(passwordConfirmation){
+    this._passwordConfirmation = passwordConfirmation
+  })
+
+userSchema
+  .pre('validate', function(next){
+    if (this.isModified('password') && this.password !== this._passwordConfirmation){
+      this.invalidate('passwordConfirmation', 'Does not match password field.')
+    }
+    next()
+  })
+
+userSchema
+  .pre('save', function(next){
+    if (this.isModified('password')){
+      this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync()) //this generates our encryption method and sets the default number of rounds, which can also be specified
+    }
+    next()
+  })
+
+userSchema.plugin(uniqueValidator)
+
+userSchema.methods.validatePassword = function(password){
+  console.log(password, this.password)
+  return bcrypt.compareSync(password, this.password)
+}
+
+export default mongoose.model('User', userSchema)
