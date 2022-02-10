976fbee2348636299db279ffa106399506b45c07
Oliver Calvey <ollie.calvey@gmail.com>
2022-02-10 15:54:45 +0000

    add child holiday cards

--- backend/controllers/holidays.js
+++ backend/controllers/holidays.js
@@ -5,7 +5,6 @@ export const addHoliday = async (req, res) => {
   try {
     // console.log('req.curentuser ----> ', req.currentUser)
     // console.log('req.body ----> ', req.body)
-
     const holidayToAdd = await Holiday.create({ ...req.body, owner: req.currentUser._id })
     return res.status(201).json(holidayToAdd)
   } catch (err) {
@@ -16,7 +15,7 @@ export const addHoliday = async (req, res) => {
 export const showAllHolidays = async (_req, res) => {
   try {
     const holidays = await Holiday.find()
-    return res.status(200).json(holidays)
+    return res.status(200).json(holidays) 
   } catch (err) {
     return res.status(404).json(err)
   }
@@ -32,8 +31,18 @@ export const getHolidayById = async (req, res) => {
   }
 }
 
-// export const addHolidayCard = async (req, res) => {
-//   const { id } = req.params
-//   const holidayCardToAdd = await HolidayType.create({ ...req.body })
-//   Holiday.findOneAndUpdate(id, { holidayTypes})
-// }
+export const addHolidayCard = async (req, res) => {
+  try {
+    const { id } = req.params
+    
+    const holidayCardToAdd = await HolidayType.create({ ...req.body, owner: req.currentUser._id })
+    console.log('holiday card ---> ', holidayCardToAdd)
+    const holiday = await Holiday.findById(id)
+    holiday.holidayTypes.push(holidayCardToAdd._id)
+    await holiday.save()
+    console.log(holiday)
+    return res.status(201).json(holiday)
+  } catch (err) {
+    return res.status(422).json(err)
+  }
+}
