ee8f82e6a935e8e7201b37ee00a496fa82be3eea
Essi Karjalainen <essi.sari.sofia@gmail.com>
2022-02-11 15:28:00 +0000

    holiday seeds

---
+++ backend/seeds/data/holidays.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: "Linda's trip to London",
+    location: "London",
+    longitude: 98,
+    latitude: 77,
+    date: "12/20",
+    description: "Fab time with the gals",
+    image: "image.com",
+  },
+  {
+    title: "Linda's trip to Bali",
+    location: "Bali",
+    longitude: 958,
+    latitude: 7227,
+    date: "9/18",
+    description: "Cocktails and sunburns with my hubby",
+    image: "image2.com",
+  }
+]
