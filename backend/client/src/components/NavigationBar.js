258f962ed460260bf006061e216388a07cec4cec
Jonny Vickers <jonathanbvickers@gmail.com>
2022-02-16 10:05:03 +0000

    fixed navigation bar to update if logged in

--- backend/client/src/components/NavigationBar.js
+++ backend/client/src/components/NavigationBar.js
@@ -1,21 +1,52 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import { userAuth } from '../enviroment/helpers/auth'
-import MenuTab from './subComponents/MenuTab'
-// import { Menu, MenuButton, IconButton, MenuList, MenuItem, Heading } from '@chakra-ui/react'
-// import { AddIcon, AtSignIcon, HamburgerIcon, LockIcon, StarIcon, SunIcon } from '@chakra-ui/icons'
+// import MenuTab from './subComponents/MenuTab'
+import { Menu, MenuButton, IconButton, MenuList, MenuItem, Heading } from '@chakra-ui/react'
+import { AddIcon, AtSignIcon, HamburgerIcon, LockIcon, StarIcon, SunIcon } from '@chakra-ui/icons'
 
 const NavigationBar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  
+  useEffect(() => {
+    setIsLoggedIn(userAuth())
+  }, )
+
+  const navigate = useNavigate()
+  const handleLogOut = () => {
+    window.localStorage.removeItem('holiday-token')
+    setIsLoggedIn(false)
+    navigate('/')
+  }
 
   return (
     <Navbar className="navbar-container">
       <Container>
         <Navbar.Brand href='/'>Holiday Mates</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        {userAuth() ?
-          <MenuTab />
+        {isLoggedIn ?
+          <Menu isLazy>
+            <MenuButton
+              as={IconButton}
+              aria-label='Options'
+              icon={<HamburgerIcon />}
+              variant='outline'
+            />
+            <MenuList>
+              <MenuItem icon={<AtSignIcon />} onClick={() => navigate('/profile')}>Profile</MenuItem>
+              <MenuItem icon={<AddIcon />} onClick={() => navigate('/')}>Add Holiday</MenuItem>
+              <MenuItem icon={<StarIcon />} onClick={() => navigate('/')}>Mates</MenuItem>
+              <MenuItem icon={<SunIcon />} onClick={() => navigate('/search')}>Explore the map</MenuItem>
+              <MenuItem icon={<LockIcon />}
+                onClick={() => {
+                  handleLogOut()
+                }}>
+                Logout
+              </MenuItem>
+            </MenuList>
+          </Menu>
           :
           <>
             <Link to="/login">Login</Link>
