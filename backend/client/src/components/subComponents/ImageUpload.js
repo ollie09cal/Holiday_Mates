99bbbb4ed138ecb2271fd07152bd2abcedc4edad
Essi Karjalainen <essi.sari.sofia@gmail.com>
2022-02-16 10:06:34 +0000

    image upload fix v2

--- backend/client/src/components/subComponents/ImageUpload.js
+++ backend/client/src/components/subComponents/ImageUpload.js
@@ -3,14 +3,15 @@ import { uploadPreset, uploadURL, apiKey, timestamp } from '../../enviroment/env
 import { FormControl, FormLabel, Input } from '@chakra-ui/react'
 import axios from 'axios'
 
+
 export const ImageUpload = (props) => {  //needs value = url, name = htmlFor&id, handleImageURL - hoisting
   const { value, name, handleImageURL } = props
+
 
   const handleUpload = async (e) => {
     const data = new FormData()
     data.append('file', e.target.files[0])
     data.append('api_key', apiKey)
-    data.append('public_id', name)
     data.append('timestamp', timestamp)
     data.append('upload_preset', uploadPreset)
     const res = await axios.post(uploadURL, data)
